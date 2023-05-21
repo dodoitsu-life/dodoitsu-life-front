b42b44ba01386cf5846326fc5d62d37cfa901c06
FAL-coffee <fal.engineer.2001@gmail.com>
2023-05-22 03:00:48 +0900

    :tada: first commit

---
+++ app/_components/Header/Header.tsx
@@ -0,0 +1,99 @@
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <nav
+      role="navigation"
+      aria-label="main navigation"
+      className="bg-primary flex"
+    >
+      <div id="navbar-brand" className="items-stretch flex flex-shrink-0 h-13">
+        <Link href="/">
+          <div className="hover:bg-primary-dark text-lg h-full flex items-center gap-1 font-noto p-1 text-white">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 25 25"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-7 h-7"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25"
+              />
+            </svg>
+            都都逸ライフ
+          </div>
+        </Link>
+      </div>
+      <div
+        id="navbar-default"
+        className="items-stretch flex justify-between flex-grow flex-shrink-0 h-13"
+      >
+        <div id="navbar-default-start" className="flex">
+          <Link href="/">
+            <div className="hover:bg-primary-dark h-full text-sm flex items-center font-noto p-1 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 28 28"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-7 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.666 3.888A2.25 2.25 0 0013.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 01-.75.75H9a.75.75 0 01-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 01-2.25 2.25H6.75A2.25 2.25 0 014.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 011.927-.184"
+                />
+              </svg>
+              最新の投稿
+            </div>
+          </Link>
+          <Link href="/">
+            <div className="hover:bg-primary-dark h-full text-sm flex items-center font-noto p-2 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 28 28"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-7 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M16.5 18.75h-9m9 0a3 3 0 013 3h-15a3 3 0 013-3m9 0v-3.375c0-.621-.503-1.125-1.125-1.125h-.871M7.5 18.75v-3.375c0-.621.504-1.125 1.125-1.125h.872m5.007 0H9.497m5.007 0a7.454 7.454 0 01-.982-3.172M9.497 14.25a7.454 7.454 0 00.981-3.172M5.25 4.236c-.982.143-1.954.317-2.916.52A6.003 6.003 0 007.73 9.728M5.25 4.236V4.5c0 2.108.966 3.99 2.48 5.228M5.25 4.236V2.721C7.456 2.41 9.71 2.25 12 2.25c2.291 0 4.545.16 6.75.47v1.516M7.73 9.728a6.726 6.726 0 002.748 1.35m8.272-6.842V4.5c0 2.108-.966 3.99-2.48 5.228m2.48-5.492a46.32 46.32 0 012.916.52 6.003 6.003 0 01-5.395 4.972m0 0a6.726 6.726 0 01-2.749 1.35m0 0a6.772 6.772 0 01-3.044 0"
+                />
+              </svg>
+              ランキング
+            </div>
+          </Link>
+        </div>
+        <div id="navbar-default-end" className="flex">
+          <Link href="/login">
+            <div className="hover:bg-primary-dark h-full text-sm flex items-center font-noto p-2 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-7 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+                />
+              </svg>
+              ログイン
+            </div>
+          </Link>
+        </div>
+      </div>
+    </nav>
+  );
+}
