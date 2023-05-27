1c51eb61947b2da72e90229ab7bd15f7b849083e
FAL-coffee <fal.engineer.2001@gmail.com>
2023-05-28 01:08:27 +0900

    :sparkles: jotai-providerの実装

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Header from "@/app/_components/Header/Header";
+import JotaiProvider from "@/app/_components/Providers/JotaiProvider/JotaiProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,10 +15,12 @@ export default function RootLayout({
   return (
     <html lang="jp">
       <body className={inter.className}>
-        <Header />
-        <div id="main-container">
-          <section>{children}</section>
-        </div>
+        <JotaiProvider>
+          <Header />
+          <div id="main-container">
+            <section>{children}</section>
+          </div>
+        </JotaiProvider>
       </body>
     </html>
   );
