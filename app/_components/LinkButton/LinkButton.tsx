206294b2ce738a76accac35f07cc164900cc274d
FAL-coffee <fal.engineer.2001@gmail.com>
2023-05-29 01:56:46 +0900

    :adhesive_bandage: 重複していた項目を削除

--- app/_components/LinkButton/LinkButton.tsx
+++ app/_components/LinkButton/LinkButton.tsx
@@ -6,7 +6,7 @@ export const LinkButton = (props: Props) => {
   return (
     <Link
       href={href}
-      className="inline-flex items-center justify-center px-3 py-1 text-lg font-medium text-white bg-primary hover:bg-primary-dark border border-transparent rounded-md shadow-sm hover:bg-primary-dark focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-dark"
+      className="inline-flex items-center justify-center px-3 py-1 text-lg font-medium text-white bg-primary hover:bg-primary-dark border border-transparent rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-primary-dark"
     >
       {text}
     </Link>
