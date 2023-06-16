0449cee80776d0a0c80d6d37a85ceb793a7cc7ba
FAL-coffee <fal.engineer.2001@gmail.com>
2023-06-17 00:54:05 +0900

    :pencil2: url

--- config/app.config.ts
+++ config/app.config.ts
@@ -2,5 +2,5 @@ export const appConfig = () => ({
   // vercelにデプロイする場合は、VERCEL_URLを環境変数に設定する
   projectUrl: process.env.NEXT_PUBLIC_VERCEL_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : "http://<",
+    : "http://localhost:3000",
 });
