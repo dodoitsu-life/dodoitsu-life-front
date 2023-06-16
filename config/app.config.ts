da21a00132484aa698822833a729c19ac71f0b43
FAL-coffee <fal.engineer.2001@gmail.com>
2023-06-16 23:54:42 +0900

    :wrench: vercel_envに変更

--- config/app.config.ts
+++ config/app.config.ts
@@ -1,6 +1,6 @@
 export const appConfig = () => ({
   // vercelにデプロイする場合は、VERCEL_URLを環境変数に設定する
   projectUrl: process.env.NEXT_PUBLIC_VERCEL_URL
-    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-    : "http://localhost:3000",
+    ? `https://${process.env.VERCEL_URL}`
+    : "http://<",
 });
