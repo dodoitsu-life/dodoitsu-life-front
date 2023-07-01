7c940c8da2fd25a8cea27799c6abf5b351c92cd7
FAL-coffee <fal.engineer.2001@gmail.com>
2023-07-02 01:57:23 +0900

    :sparkles: OGPの動的作成

--- app/(dodoitsu)/dodoitsu/detail/[id]/page.tsx
+++ app/(dodoitsu)/dodoitsu/detail/[id]/page.tsx
@@ -1,9 +1,24 @@
 import { getDodoitsuById } from "@/src/server/dodoitsu/getDodoitsuById";
 import { DodoitsuCard } from "../../../_components/DodoitsuCard";
+import seoGen from "@/src/utils/seoGen";
 
 type Params = {
   id: string;
 };
+
+export async function generateMetadata({ params }: { params: Params }) {
+  const { dodoitsu } = await getDodoitsuById({ id: params.id });
+
+  // TODO: pathの動的な取得が現状できないので、一旦固定値を入れておく
+  const url = "https://dodoitsu.vercel.app";
+
+  return seoGen({
+    title: `${dodoitsu.content} | 都々逸ライフ`,
+    description: dodoitsu.description,
+    url: `${url}/dodoitsu/detail/${dodoitsu.id}`,
+    imageUrl: `${url}/api/og?title=${dodoitsu.content}`,
+  });
+}
 
 export default async function DodoitsuDetail({ params }: { params: Params }) {
   const { dodoitsu } = await getDodoitsuById({ id: params.id });
---
+++ src/utils/seoGen.ts
@@ -0,0 +1,43 @@
+import type { Metadata } from "next";
+
+type Params = {
+  title: string;
+  description?: string;
+  url: string;
+  imageUrl: string;
+};
+
+export default function seoGen({
+  title,
+  description,
+  url,
+  imageUrl,
+}: Params): Metadata {
+  const metadata: Metadata = {
+    title: title,
+    description: description,
+    icons: "/favicon.ico",
+    keywords: ["都々逸", "都々逸ライフ", title],
+    viewport: {
+      width: "device-width",
+      initialScale: 1,
+      maximumScale: 1,
+    },
+    twitter: {
+      card: "summary_large_image",
+      images: [imageUrl],
+    },
+    openGraph: {
+      title: title,
+      description: description,
+      url: url,
+      siteName: "都々逸ライフ",
+      images: {
+        url: imageUrl,
+        width: 1200,
+        height: 600,
+      },
+    },
+  };
+  return metadata;
+}
