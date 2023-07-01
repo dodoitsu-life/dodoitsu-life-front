1ee6ff3c7e1d730738ced2711345589d6e83d0d0
FAL-coffee <fal.engineer.2001@gmail.com>
2023-07-01 23:26:41 +0900

    :alien: API接続

--- src/server/dodoitsu/createDodoitsu.ts
+++ src/server/dodoitsu/createDodoitsu.ts
@@ -1,8 +1,9 @@
-// import { useAxios } from "@/src/hooks/useAxios";
+import { useAxios } from "@/src/hooks/useAxios";
+import { Dodoitsu } from "@/src/types/Dodoitsu";
 
 type CreateDodoitsuRequest = {
   content: string;
-  comment: string;
+  description: string;
 };
 
 type CreateDodoitsuResponse = { id: string };
@@ -10,20 +11,14 @@ type CreateDodoitsuResponse = { id: string };
 export const createDodoitsu = async (
   params: CreateDodoitsuRequest
 ): Promise<CreateDodoitsuResponse> => {
-  //   const $axios = useAxios();
-  //   const id: string = await $axios
-  //     .post(`/dodoitsu`, {
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify(params),
-  //     })
-  //     .then((response) => {
-  //       return response.data.id;
-  //     })
-  //     .catch((error) => {
-  //       throw new Error(error);
-  //     });
-  const id: string = "testId";
-  return { id };
+  const $axios = useAxios();
+  const { data: dodoitsu }: { data: Dodoitsu } = await $axios
+    .post(`/dodoitsu`, params)
+    .then((response) => {
+      return response.data;
+    })
+    .catch((error) => {
+      throw new Error(error);
+    });
+  return { id: dodoitsu.id };
 };
