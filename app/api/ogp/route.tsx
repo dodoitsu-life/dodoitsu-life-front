e0d3bb4bca1ea70e1d1f7c5f21d28749b40e7bba
ふぁる <73627117+FAL-coffee@users.noreply.github.com>
2023-07-02 22:59:47 +0900

    :sparkles: ogp動的作成を実装 (#11)

---
+++ app/api/ogp/route.tsx
@@ -0,0 +1,38 @@
+import { NextRequest } from "next/server";
+import { ImageResponse } from "@vercel/og";
+
+// 動的レンダリングを強制する
+export const dynamic = "force-dynamic";
+
+export const runtime = "edge";
+
+const font = fetch(
+  new URL("../../../assets/font/IBMPlexSansJPSubset.woff", import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export async function GET(req: NextRequest) {
+  const searchParams = new URL(req.url).searchParams;
+  const content = searchParams.get("content");
+
+  const fontData = await font;
+
+  return new ImageResponse(
+    (
+      <div tw="bg-violet-200 flex h-600px w-1200px justify-center">
+        <div tw="bg-white self-center justify-center rounded-lg flex w-1100px h-500px">
+          <div tw="text-5xl flex self-center text-gray-900 m-10">{content}</div>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 600,
+      fonts: [
+        {
+          name: "IBMPlexSansJPSubset",
+          data: fontData,
+        },
+      ],
+    }
+  );
+}
