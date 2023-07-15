69a645d086a068ffe04814b7526debeaf1878e93
FAL-coffee <fal.engineer.2001@gmail.com>
2023-07-15 22:59:12 +0900

    :zap: 一覧画面にて、2ページがない場合はページネーションを非表示

--- app/(dodoitsu)/dodoitsu/(list)/ranking/page.tsx
+++ app/(dodoitsu)/dodoitsu/(list)/ranking/page.tsx
@@ -35,7 +35,13 @@ export default async function DodoitsuRanking({
 
         <DodoitsuList dodoitsuList={dodoitsuList} />
       </section>
-      <PaginationLink page={page} maxPage={maxPage} pageLinkGen={pageLinkGen} />
+      {maxPage > 1 && (
+        <PaginationLink
+          page={page}
+          maxPage={maxPage}
+          pageLinkGen={pageLinkGen}
+        />
+      )}
     </div>
   );
 }
