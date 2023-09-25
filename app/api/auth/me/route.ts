ff62f5c102d13d8486af43f9350f5ce3301fe4cd
FAL-coffee <fal.engineer.2001@gmail.com>
2023-09-26 01:31:30 +0900

    :adhesive_bandage: 動的レンダリングの強制

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -1,6 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getMe } from "@/src/server/auth/me";
 
+// 動的レンダリングを強制する
+export const dynamic = "force-dynamic";
+
 export async function GET(_: NextRequest) {
   return await getMe()
     .then((response) => {
