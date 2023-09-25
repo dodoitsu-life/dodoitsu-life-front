c8d882053bc1f280fbdbf8e1bb34898d3fd2a90c
FAL-coffee <fal.engineer.2001@gmail.com>
2023-09-26 02:22:27 +0900

    :adhesive_bandage: no-cacheをaxiosのheaderに追加

--- src/server/auth/me.ts
+++ src/server/auth/me.ts
@@ -4,7 +4,13 @@ import { User } from "../../types/User";
 type GetMeResponse = { user: User } | null;
 
 export const getMe = async (): Promise<GetMeResponse> => {
-  return await $axios.get("/auth/me").then((response) => {
-    return response.data;
-  });
+  return await $axios
+    .get("/auth/me", {
+      headers: {
+        "Cache-Control": "no-store",
+      },
+    })
+    .then((response) => {
+      return response.data;
+    });
 };
