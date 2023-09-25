2ea3266697e8bc437405f43515344826f82ce95a
FAL-coffee <fal.engineer.2001@gmail.com>
2023-09-26 02:06:48 +0900

    :adhesive_bandage: edge

---
+++ app/api/user/me/route.ts
@@ -0,0 +1,16 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getMe } from "@/src/server/auth/me";
+
+// 動的レンダリングを強制する
+export const dynamic = "force-dynamic";
+export const runtime = "edge";
+
+export async function GET(_: NextRequest) {
+  return await getMe()
+    .then((response) => {
+      return NextResponse.json({ ...response }, { status: 200 });
+    })
+    .catch((e) => {
+      return NextResponse.json({ error: e.statusText }, { status: e.status });
+    });
+}
