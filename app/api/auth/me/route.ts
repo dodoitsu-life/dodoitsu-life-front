1bfc2a3acf32011b2bd57215a5bdf6538197e61c
FAL-coffee <fal.engineer.2001@gmail.com>
2023-09-26 00:16:38 +0900

    :alembic: statusを追加

--- app/api/auth/me/route.ts
+++ app/api/auth/me/route.ts
@@ -4,7 +4,7 @@ import { getMe } from "@/src/server/auth/me";
 export async function GET(_: NextRequest) {
   return await getMe()
     .then((response) => {
-      return NextResponse.json(response);
+      return NextResponse.json({ ...response }, { status: 200 });
     })
     .catch((e) => {
       return NextResponse.json({ error: e.statusText }, { status: e.status });
