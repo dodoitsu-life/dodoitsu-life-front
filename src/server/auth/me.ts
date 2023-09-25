610d71520448b10d579f3debbc918540c0cc0e7e
FAL-coffee <fal.engineer.2001@gmail.com>
2023-09-26 00:49:50 +0900

    :adhesive_bandage: cacheを解除

--- src/server/auth/me.ts
+++ src/server/auth/me.ts
@@ -1,11 +1,10 @@
 import $axios from "@/src/lib/axios";
-import { cache } from "react";
 import { User } from "../../types/User";
 
 type GetMeResponse = { user: User } | null;
 
-export const getMe = cache(async (): Promise<GetMeResponse> => {
+export const getMe = async (): Promise<GetMeResponse> => {
   return await $axios.get("/auth/me").then((response) => {
     return response.data;
   });
-});
+};
