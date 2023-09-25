a1b632864886ebf38060887ecf646da832f5739e
FAL-coffee <fal.engineer.2001@gmail.com>
2023-09-26 00:29:27 +0900

    :alembic: getMeのコードをuseQueryに移植させてみる

--- app/auth/init/page.tsx
+++ app/auth/init/page.tsx
@@ -3,35 +3,39 @@ import { useQuery } from "react-query";
 import { useRouter } from "next/navigation";
 import { useContext } from "react";
 import { AuthContext } from "@/app/_components/Providers/AuthProvider";
-import { User } from "@/src/types/User";
-
-const getMe = async (): Promise<User> => {
-  const res = await fetch("/api/auth/me", {
-    credentials: "include",
-  });
-
-  if (!res.ok) {
-    throw new Error("ログインに失敗しました");
-  }
-
-  const resJson = await res.json();
-  return resJson.user;
-};
 
 export default function Page() {
   const router = useRouter();
 
   const { logIn } = useContext(AuthContext);
-  useQuery("me", getMe, {
-    onSuccess: async (data) => {
-      await logIn(data);
-      router.push("/");
+  useQuery(
+    "me",
+    async () => {
+      const res = await fetch("/api/auth/me", {
+        credentials: "include",
+      });
+      console.log(res);
+      console.log(await res.json);
+
+      if (!res.ok) {
+        throw new Error("ログインに失敗しました");
+      }
+
+      const resJson = await res.json();
+      console.log(resJson.data);
+      return resJson.data;
     },
-    onError: (error: Error) => {
-      alert(error.message);
-      router.push("/");
-    },
-  });
+    {
+      onSuccess: async (res) => {
+        await logIn(res);
+        router.push("/");
+      },
+      onError: (error: Error) => {
+        alert(error.message);
+        router.push("/");
+      },
+    }
+  );
 
   return <div>しばらくお待ちください</div>;
 }
