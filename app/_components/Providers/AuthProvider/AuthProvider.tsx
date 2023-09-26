9e4a62de1a14cd3ca31b725bab52bfd22a3bc956
FAL-coffee <fal.engineer.2001@gmail.com>
2023-09-27 03:17:32 +0900

    :adhesive_bandage: contextで毎回meする

--- app/_components/Providers/AuthProvider/AuthProvider.tsx
+++ app/_components/Providers/AuthProvider/AuthProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Cookies from "js-cookie";
-import React, { createContext, useState, useEffect } from "react";
+import React, { createContext, useState } from "react";
+import { useQuery } from "react-query";
 import { User } from "@/src/types/User";
 import { AuthContextType } from "./types";
 
@@ -8,17 +9,30 @@ const AuthContext = createContext<AuthContextType>({} as AuthContextType);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
   const [user, setUser] = useState<User | null>(null);
-
-  useEffect(() => {
-    const storedUser = localStorage.getItem("user");
-    if (storedUser === "undefined") {
+  useQuery(
+    "me",
+    async () => {
       localStorage.removeItem("user");
-      return;
+      const res = await fetch("/api/auth/me", {
+        credentials: "include",
+        cache: "no-cache",
+      });
+      if (!res.ok) {
+        throw new Error("ログインに失敗しました");
+      }
+
+      const resJson = await res.json();
+      return resJson.data;
+    },
+    {
+      onSuccess: (data) => {
+        logIn(data);
+      },
+      onError: () => {
+        alert("ログインに失敗しました。\nログインし直してください。");
+      },
     }
-    if (storedUser) {
-      setUser(JSON.parse(storedUser));
-    }
-  }, []);
+  );
 
   const logIn = async (userData: User) => {
     await localStorage.setItem("user", JSON.stringify(userData));
