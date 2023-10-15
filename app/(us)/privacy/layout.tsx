e68ffe04778ccdb50496549973b6d4e05e5fd5a8
FAL-coffee <fal.engineer.2001@gmail.com>
2023-10-15 20:26:44 +0900

    :adhesive_bandage: 変更履歴の表示

--- app/(us)/privacy/layout.tsx
+++ app/(us)/privacy/layout.tsx
@@ -1,6 +1,15 @@
 import { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "都々逸ライフ｜プライバシーポリシー",
+import seoGen from "@/src/utils/seoGen";
+
+export const metadata = (): Metadata => {
+  // TODO: pathの動的な取得が現状できないので、一旦固定値を入れておく
+  const url = "https://dodoitsu-life.vercel.app";
+  return seoGen({
+    title: "都々逸ライフ｜プライバシーポリシー",
+    description: "プライバシーポリシーを表示します",
+    url: `${url}/privacy`,
+    imageUrl: `${url}/api/ogp?content=都々逸ライフ\nプライバシーポリシー`,
+  });
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
