35654870e58619326906428b8d1b0f249d7c01f1
KEINA0109O\Keina_S <osamu.engineer.2001@gmail.com>
2023-12-16 18:25:42 +0900

    healthページの削除

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,14 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Metadata } from "next";
 import Head from "./head";
 import { Header } from "@components/Header";
 import { Footer, type Item as FooterItem } from "@components/Footer";
 import { JotaiProvider } from "@components/Providers/JotaiProvider/JotaiProvider";
 import { ReactQueryProvider } from "@components/Providers/ReactQueryProvider";
 import { AuthProvider } from "./_components/Providers/AuthProvider";
-import seoGen from "@/src/utils/seoGen";
 import { appConfig } from "@/src/config/app.config";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata = (): Metadata => {
-  // TODO: pathの動的な取得が現状できないので、一旦固定値を入れておく
-  const url = "https://dodoitsu-life.vercel.app";
-  return seoGen({
-    title: "都々逸ライフ",
-    description: "都々逸を投稿・閲覧できるサービスです",
-    url: `${url}/`,
-    imageUrl: `${url}/api/ogp?content=都々逸ライフ\n都々逸の投稿・閲覧サイト`,
-  });
-};
 
 export default function RootLayout({
   children,
