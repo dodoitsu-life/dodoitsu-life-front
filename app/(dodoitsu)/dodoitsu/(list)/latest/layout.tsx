fa0918fa77ca793f586b89ca4643bae327b99779
KEINA0109O\Keina_S <osamu.engineer.2001@gmail.com>
2023-12-19 20:46:20 +0900

    SEO対策 既存のタイトルタグのサイト名とページ名を逆にする

--- app/(dodoitsu)/dodoitsu/(list)/latest/layout.tsx
+++ app/(dodoitsu)/dodoitsu/(list)/latest/layout.tsx
@@ -5,7 +5,7 @@ export const metadata = (): Metadata => {
   // TODO: pathの動的な取得が現状できないので、一旦固定値を入れておく
   const url = "https://dodoitsu-life.vercel.app";
   return seoGen({
-    title: "都々逸ライフ｜最近投稿された都々逸",
+    title: "最近投稿された都々逸｜都々逸ライフ",
     description: "最近投稿された都々逸を表示します",
     url: `${url}/dodoitsu/latest`,
     imageUrl: `${url}/api/ogp?content=最近投稿された都々逸`,
--- app/(us)/privacy/layout.tsx
+++ app/(us)/privacy/layout.tsx
@@ -5,7 +5,7 @@ export const metadata = (): Metadata => {
   // TODO: pathの動的な取得が現状できないので、一旦固定値を入れておく
   const url = "https://dodoitsu-life.vercel.app";
   return seoGen({
-    title: "都々逸ライフ｜プライバシーポリシー",
+    title: "プライバシーポリシー｜都々逸ライフ",
     description: "プライバシーポリシーを表示します",
     url: `${url}/privacy`,
     imageUrl: `${url}/api/ogp?content=都々逸ライフ\nプライバシーポリシー`,
