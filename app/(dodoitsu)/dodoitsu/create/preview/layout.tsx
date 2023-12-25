f61dd3395b5aa5a4c05345d638b6ff06762dcce5
KEINA0109O\Keina_S <osamu.engineer.2001@gmail.com>
2023-12-25 14:34:52 +0900

    都々逸作成プレビュー画面を検索結果に表示しない

--- app/(dodoitsu)/dodoitsu/create/preview/layout.tsx
+++ app/(dodoitsu)/dodoitsu/create/preview/layout.tsx
@@ -1,6 +1,10 @@
 import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "都々逸ライフ｜プレビュー",
+
+  robots: {
+    index: false, // noindexの設定
+  },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
