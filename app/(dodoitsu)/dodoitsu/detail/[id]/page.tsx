2c7acaac4f15a2ed7535bd2e56494b0fc9905eff
ふぁる <73627117+FAL-coffee@users.noreply.github.com>
2024-01-06 21:55:59 +0900

    :adhesive_bandage: AuthorNameにundefined文字列が渡されていたバグの修正 (#80)

--- app/(dodoitsu)/dodoitsu/detail/[id]/page.tsx
+++ app/(dodoitsu)/dodoitsu/detail/[id]/page.tsx
@@ -11,12 +11,13 @@ export async function generateMetadata({ params }: { params: Params }) {
     const dodoitsu = res.dodoitsu;
     // TODO: pathの動的な取得が現状できないので、一旦固定値を入れておく
     const url = "https://dodoitsu-life.vercel.app";
+    const authorName = dodoitsu.author ? dodoitsu.author.twitterId : "";
 
     return seoGen({
       title: `${dodoitsu.content}｜都々逸ライフ`,
       description: dodoitsu.description,
       url: `${url}/dodoitsu/detail/${dodoitsu.id}`,
-      imageUrl: `${url}/api/ogp?content=${dodoitsu.content}&color_code=D1BCF7&author_name=${dodoitsu.author?.twitterId}`,
+      imageUrl: `${url}/api/ogp?content=${dodoitsu.content}&color_code=D1BCF7&author_name=${authorName}`,
     });
   });
 }
